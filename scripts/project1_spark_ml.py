4f80c09a30f9acd7d702a15f14379b43ebd047c4
adrian <adrian.adriano.tech@gmail.com>
2024-04-11 09:35:11 -0400

    feat(project): add init files

---
+++ scripts/project1_spark_ml.py
@@ -0,0 +1,13 @@
+from pyspark.sql import SparkSession
+import pandas as pd
+
+
+
+
+spark = SparkSession.builder.appName("Datacamp Pyspark Tutorial").config("spark.memory.offHeap.enabled","true").config("spark.memory.offHeap.size","10g").getOrCreate()
+
+# Load data from specified path "csv file"
+data_path = "/app/data/online_retail.csv"
+df = spark.read.csv(data_path,header=True,escape="\"")
+result = df.show(5,0)
+print(result)
